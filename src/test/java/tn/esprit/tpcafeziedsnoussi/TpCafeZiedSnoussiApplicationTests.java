a72d6346ef765b5de13809cba94d4a38ae696028
Zied Snoussi <z.snoussi@as-solutions.fr>
2025-10-25 19:29:44 +0100

    Add test profile configuration and update Maven build process

--- src/test/java/tn/esprit/tpcafeziedsnoussi/TpCafeZiedSnoussiApplicationTests.java
+++ src/test/java/tn/esprit/tpcafeziedsnoussi/TpCafeZiedSnoussiApplicationTests.java
@@ -2,8 +2,12 @@ package tn.esprit.tpcafeziedsnoussi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class TpCafeZiedSnoussiApplicationTests {
 
     @Test
